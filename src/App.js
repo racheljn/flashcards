fa34826e5c19fa64273f1d7153f1af747570a135
rachel nguyen <rnguye12@lion.lmu.edu>
2020-06-30 04:36:38 -0500

    Added components and basic Q/A flip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
   return (
-    <h1>Flashcards</h1>
+    <FlashcardList flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: '안녕',
+    answer: 'hello',
+    options: [
+      'hello',
+      'yellow',
+      'why',
+      'no'
+    ]
+  },
+  {
+    id: 2,
+    question: '네',
+    answer: 'yes',
+    options: [
+      'yes',
+      'no',
+      'maybe',
+      'so'
+    ]
+  }
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function Flashcard({ flashcard }) {
+    const [flip, setFlip] = useState(false)
+
+    return (
+        <div onClick ={() => setFlip(!flip)}>
+            {flip ? flashcard.answer : flashcard.question}
+        </div>
+    )
+}
